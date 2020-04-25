8346bd60b8236d21b72d47c66f204523c18a38e3
bricksy10 <smckay10@gmail.com>
2020-04-25 15:47:02 -0400

    curl signup/create

---
+++ curl-scripts/sign-up.sh
@@ -0,0 +1,13 @@
+curl "https://bricksy10.github.io/tic-tac-toe/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
